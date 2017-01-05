1a1e8da0b5b00494abcb00acbd000ad431b8024a
Wander Hillen <wjw.hillen@gmail.com>
2017-01-05 12:14:49 +0100

    Got background hting working

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Bib
+import Control.Monad
+import Control.Monad.IO.Class
+import Network.Wai.Middleware.RequestLogger
+import Web.Scotty
+
+import ImageProvider
 
 main :: IO ()
 main = do
-    print "Piglet!"
-    someFunc
+    getBackgroundPath <- startBackgroundProvider --returns a function that returns a image filepath
+    scotty 3000 $ do
+        middleware logStdoutDev
+        get "/" showLandingPage  
+        get "/background" $ serveBackground getBackgroundPath
+        --post "/upload"
+        --post "/download"
+        
+showLandingPage :: ActionM () 
+showLandingPage = do
+    setHeader "Content-Type" "text/html" --file doesn't set the content type by itself
+    file "homepage.html" --body of the response is a file (in this case the homepage)
+
+serveBackground :: (IO FilePath) -> ActionM ()
+serveBackground getBackgroundPath = do
+    filepath <- liftIO getBackgroundPath --see the ImageProvider module for how this works
+    setHeader "Content-Type" "image/jpeg" -- all the images are JPEGs
+    file filepath --serve the background image
+    
+-- /upload
--- src/ImageProvider.hs
+++ src/ImageProvider.hs
@@ -1,10 +1,29 @@
-module Bib
-    ( someFunc
-    ) where
+module ImageProvider where
 
+--    ( getImage
+--    ) where
+
+import Control.Applicative
+import Control.Concurrent
+import Control.Monad
 import System.Directory
 
-someFunc :: IO ()
-someFunc = do
-    here <- getCurrentDirectory
-    putStrLn here
+imageDirectory = "backgrounds/"
+
+--interface to the main app. It is intended to be easily refactored for more arguments
+--when I want to select a background based on screen size or whatnot.
+--returns a function that returns a filepath
+startBackgroundProvider :: IO (IO FilePath)
+startBackgroundProvider = do
+    mv <- newEmptyMVar
+    forkIO $ backgroundProvider mv
+    return (takeMVar mv)
+
+--Makes sure that the supplied MVar is "always" filled with a filename to a background image
+--Currently serves images in a round robin fashion from a directory full of nice images
+backgroundProvider :: MVar FilePath -> IO ()
+backgroundProvider mv = do
+    filenames <- cycle . (map (imageDirectory ++ )) <$> listDirectory imageDirectory --infinite list of filenames
+    forM_ filenames $ \f -> putMVar mv f --put filename in MVar, when someone empties the MVar, put another one in
+    --because filenames is an infinite list, this loops forever, laziness makes sure that memory usage
+    --stays under control
