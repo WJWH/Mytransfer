92d22cc81c6ae0669d553d165d481a5de2835459
Wander Hillen <wjw.hillen@gmail.com>
2017-01-17 17:09:18 +0100

    Finished poc autoscaler and updated README.

--- README.md
+++ README.md
@@ -4,14 +4,16 @@ A clone of wetransfer.com in Haskell, just to see how it works and if I can do i
 ## Project goals
 * Use a cloud service as a storage backend to handle file storage (S3, GCS, etc). Scaling is nice.
 * Allow for "proper" autoscaling. Users should never notice a server being spun down, so it should at least have nice connection draining. The load balancers of AWS and GCE only allow connection draining for 3600 seconds though, which may not be enough for users downloading really large files on really slow connections, so it should be implemented on the servers itself.
-* Make backgrounds scale to the device size of the user. What is the point of having nice images as backgrounds if the user can't see them well?
-* Log all the things. Haskell logging is a bit all over the place, what with it being impure and all. Status: unstarted, though it uses the standard WAI middleware for logging requests at least.
+* Make backgrounds scale to the device size of the user. What is the point of having nice images as backgrounds if the user can't see them well? 
+* Log all the things. Haskell logging is a bit all over the place, what with it being impure and all. Status: it currently uses the standard WAI middleware for logging requests, but since that logs all requests the log gets flooded with tons and tons of health checks from the load balancer. It also doesn't log anything besides incoming requests.
+* Implement proper testing with hSpec. Status: not started yet.
 
 ## NOT project goals
 * Actually run a cloud storage service. It sounds like a lot of hassle. Don't rely on the website actually being up.
 * Make a super nice frontend using all the hippest technologies. In this project I will be focusing on the backend and only adding as much frontend as is required to make the application work.
+* Make everything utra-secure. Adding Let's Encrypt or something would be trivial and not a good use of time for this project.
 
 ## Personal goals
 * Write a medium complexity webapp using Scotty.
-* Figure out the Google Cloud Storage APIs.
+* Figure out the Google Cloud platform and APIs.
 
--- devops/poc_autoscaler.py
+++ devops/poc_autoscaler.py
@@ -4,6 +4,7 @@
 
 from subprocess import check_output
 import requests
+import uuid
 
 serverCapacity = 62500000 # in bytes/sec, set to actual capacity when deciding on an instance type to use
 #current value is for f1-micro instances
@@ -22,30 +23,42 @@ def getLoad(servername):
     r = requests.get('http://'+servername+'/load')
     return int(r.text)
 
+def createServer():
+    name = 'mytransfer-fs-'+uuid.uuid4().hex #generate unique name
+    check_output(["gcloud", "compute", "instances", "delete", name, "--image", "mytransfer-fileserver"])
+    check_output(["gcloud", "compute", "instance-groups", "unmanaged", "add-instances", "mytransfer-fileservers", "--instances", name])
+    return name
+    
+def getDraintime(servername):
+    r = requests.get('http://'+servername+'/timetodrain')
+    return int(r.text)
 
 drainingServers = {}
 while(True):
     fileservers = listFileservers() # returns a dict 
     #fileservers contains PROVISIONING, RUNNING and TERMINATED instances
     terminatedServers = [k for k, v in fileservers.items() if v == 'TERMINATED'] # only those which are TERMINATED
+    #delete the servers which have finished draining
     for s in terminatedServers:
         deleteServer(s)
     runningServers = {k for k, v in fileservers.items() if v == 'RUNNING'} # only those which are RUNNING
     activeServers = runningServers - drainingServers
+    #determine the load on the servers
     load = 0
     capacity = 0
     for s in activeServers:
         load += getLoad(s)
         capacity += serverCapacity //assuming all instances are equal size this is a constant
     averageload = load/capacity
-    if averageload > 0.8:
-        '''
-        //this scales up one at a time, it is also possible to scale up multiple servers at a time if load is super high
-        name = generateNewInstanceName
-        gcloud compute instances create name
-    if averageload < 0.6: #load is low, kill an instance unless it's the last one
-        candidates = []
+    if averageload > 0.8: # load is high, add another server
+        # this scales up one at a time, it is also possible to scale up multiple servers at a time 
+        # if load is super high
+        createServer() # add a new server, this call only returns after the instance has been created and added to the instance group
+    if (averageload < 0.5) and (len()>1): #load is low, kill an instance unless it's the last one
+        currentMinTimeToDrain = float('inf') # quite long
         for s in activeServers:
-            edt = curl (s + "/timetodrain") 
-            candidates.append((s,edt))
-'''
+            edt = getDraintime(s)
+            if edt < currentMinTimeToDrain:
+                serverToDrain = s
+        drainServer(serverToDrain)
+        
